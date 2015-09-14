53381c9bbdf7294945d635fea6ed610cec8895d4
ayunav <ayunanyc@gmail.com>
2015-09-14 16:43:36 -0400

    fist preliminary commit. The most basic requirements are done. Works but not perfect.

--- PickAPic/PickAPic/ViewController.m
+++ PickAPic/PickAPic/ViewController.m
@@ -14,5 +14,38 @@
 
 @implementation ViewController
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+
+-(void)viewDidAppear:(BOOL)animated {
+    if (self.imageView.image == nil) {
+        [super viewDidAppear:animated];
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc]init];
+        imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+        imagePickerController.delegate = self;
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary; 
+        [self presentViewController:imagePickerController animated:NO completion:nil];
+    }
+    else {
+        
+    }
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    self.imageView.image = image;
+    [self dismissViewControllerAnimated:YES completion:nil]; 
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
 
 @end
