638e3311266f0a73bafa6e6cb8d1bb7a49e25c68
ericysze <ericysze@gmail.com>
2015-09-16 19:21:36 -0400

    Finished Pick-a-pic, working on Part Two: Social

---
+++ PickAPic/PickAPic/CameraRollViewController.m
@@ -0,0 +1,61 @@
+//
+//  CameraRollViewController.m
+//  PickAPic
+//
+//  Created by Eric Sze on 9/16/15.
+//  Copyright © 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "CameraRollViewController.h"
+
+@interface CameraRollViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) UIImagePickerController *imagePicker;
+@property (strong, nonatomic) IBOutlet UIImageView *myImage;
+@property (weak, nonatomic) IBOutlet UIButton *cameraRollButton;
+
+
+
+@end
+
+@implementation CameraRollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.imagePicker = [[UIImagePickerController alloc] init];
+    self.imagePicker.delegate = self;
+
+}
+
+- (IBAction)camerRollButtonTapped:(UIButton *)sender {
+    
+    [self presentViewController:self.imagePicker animated:YES completion:nil];
+    
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        self.imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    } else {
+        self.imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        
+    }
+ 
+
+    
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+
+    
+    self.myImage.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    //self.myImage.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
