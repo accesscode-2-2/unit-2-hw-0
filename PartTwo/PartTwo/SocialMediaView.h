e5e26afa36e3cdf3fde9478b5174f02d98db21a1
Eric Sanchez <ericsanchez@outlook.com>
2015-09-17 16:08:18 -0400

    Connected buttons

---
+++ PartTwo/PartTwo/SocialMediaView.h
@@ -0,0 +1,13 @@
+//
+//  SocialMediaView.h
+//  PartTwo
+//
+//  Created by Eric Sanchez on 9/17/15.
+//  Copyright © 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialMediaView : UIView
+
+@end
---
+++ PartTwo/PartTwo/SocialMediaView.m
@@ -0,0 +1,28 @@
+//
+//  SocialMediaView.m
+//  PartTwo
+//
+//  Created by Eric Sanchez on 9/17/15.
+//  Copyright © 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "SocialMediaView.h"
+
+@implementation SocialMediaView
+
+- (IBAction)commentButtonTapped:(UIButton*)sender{
+	
+}
+
+- (IBAction)likeButtonTapped:(UIButton*)sender{
+	
+}
+
+- (IBAction)shareButtonTapped:(UIButton*)sender{
+	
+}
+
+
+@end
+
+
