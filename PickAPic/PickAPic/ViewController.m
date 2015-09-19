f3e075c747fa0f92ab5e1f20161fe8b1aa2ea198
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-09-18 21:20:14 -0400

    Functional Image Picker

--- PickAPic/PickAPic/ViewController.m
+++ PickAPic/PickAPic/ViewController.m
@@ -14,5 +14,31 @@
 
 @implementation ViewController
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    if (self.imageView.image == nil) {
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+        imagePickerController.delegate = self;
+        [self presentViewController:imagePickerController animated:NO completion:nil];
+    }
+    else {
+        
+    }
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(nonnull NSDictionary<NSString *,id> *)info {
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    self.imageView.image = image;
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
